482968aec39e3c1286173e74e6fae0ab2af20a1d
Hiepduong040 <hiepduong0409@gmail.com>
2024-10-14 07:25:10 +0700

    add

---
+++ client/src/App.vue
@@ -0,0 +1,12 @@
+<<template>
+  <div>
+    <TodoList/>
+  </div>
+</template>
+
+<script setup>
+import TodoList from "./components/todo-list/TodoList.vue";
+</script>
+
+<style>
+</style>>
---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>Student Detail</h2>
+    <input type="number" v-model="studentId" placeholder="Enter Student ID" />
+    <button @click="getStudentById">Get Student</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+
+const studentId = ref();
+
+const getStudentById = async () => {
+  try {
+    const res = await axios.get(`http://localhost:3000/users?id=${studentId.value}`);
+    if (res.data.length > 0) {
+      console.log(res.data[0]);
+    } else {
+      console.log("Không tìm thấy bản ghi");
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+</script>
+
+<style>
+</style>
